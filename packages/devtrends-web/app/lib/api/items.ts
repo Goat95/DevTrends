68b6b22e03a489d91bf1186af03189e71a706fae
Goat95 <hjh9703@naver.com>
2023-04-12 04:21:21 +0900

    feat: implement infinite scroll

--- packages/devtrends-web/app/lib/api/items.ts
+++ packages/devtrends-web/app/lib/api/items.ts
@@ -1,13 +1,23 @@
 import { client } from "../client";
 import { type GetItemsResult, type Item } from "./types";
+import qs from "qs";
 
 export async function createItem(params: CreateItemParams) {
   const response = await client.post<Item>("/api/items", params);
   return response.data;
 }
 
-export async function getItems() {
-  const response = await client.get<GetItemsResult>("/api/item");
+export async function getItems(cursor?: number) {
+  const response = await client.get<GetItemsResult>(
+    "/api/items".concat(
+      qs.stringify(
+        { cursor },
+        {
+          addQueryPrefix: true,
+        }
+      )
+    )
+  );
   return response.data;
 }
 
