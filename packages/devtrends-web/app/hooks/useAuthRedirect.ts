b99b4872b59474c0c29371ee6dd5b2a8cc243395
Goat95 <hjh9703@naver.com>
2023-04-19 22:39:42 +0900

    fix: shows error message properly on login failure

--- packages/devtrends-web/app/hooks/useAuthRedirect.ts
+++ packages/devtrends-web/app/hooks/useAuthRedirect.ts
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import { useActionData, useNavigate, useSearchParams } from "@remix-run/react";
 import { type AuthResult } from "~/lib/api/auth";
+import { type CatchValue } from "@remix-run/react/dist/transition";
 
 export function useAuthRedirect() {
-  const authResult = useActionData<AuthResult>();
+  const authResult = useActionData<AuthResult | CatchValue>();
   const [searchParams] = useSearchParams();
-  const next = searchParams.get("next") || "/";
+  const next = searchParams.get("next") ?? "/";
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!authResult) return;
+    if ("status" in authResult) return;
     navigate(next);
   }, [authResult, navigate, next]);
 }
