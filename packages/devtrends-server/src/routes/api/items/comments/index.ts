f7590d3c2694b1941767a6270b41142732f8c9d3
Goat95 <hjh9703@naver.com>
2023-04-23 01:05:01 +0900

    feat: initialize comments api

--- packages/devtrends-server/src/routes/api/items/comments/index.ts
+++ packages/devtrends-server/src/routes/api/items/comments/index.ts
@@ -1,23 +1,40 @@
 import { FastifyPluginAsync } from 'fastify'
 import { CommentsRoute, CommentsRouteSchema } from './schema'
+import { createAuthorizedRoute } from '../../../../plugins/requireAuthPlugin'
 
 export const commentsRoute: FastifyPluginAsync = async (fastify) => {
   fastify.get<CommentsRoute['GetComments']>(
     '/',
     { schema: CommentsRouteSchema.GetComments },
-    async (request) => {
-      return 'hello world'
-    },
+    async (request) => {},
   )
 
+  fastify.get<CommentsRoute['GetSubcomments']>(
+    '/:commentId/subcomments',
+    async () => {},
+  )
+
+  fastify.register(authorizedCommentsRoute)
+}
+
+const authorizedCommentsRoute = createAuthorizedRoute(async (fastify) => {
   fastify.post<CommentsRoute['CreateComment']>(
     '/',
     { schema: CommentsRouteSchema.CreateComment },
-    async (request) => {
-      const { id } = request.params
-      const { text } = request.body
-    },
+    async (request) => {},
   )
 
-  fastify.get<CommentsRoute['GetSubcomments']>('/:commentId', async () => {})
-}
+  fastify.post<CommentsRoute['LikeComment']>(
+    '/:commentId/likes',
+    async () => {},
+  )
+
+  fastify.delete<CommentsRoute['UnlikeComment']>(
+    '/:commentId/likes',
+    async () => {},
+  )
+
+  fastify.delete<CommentsRoute['DeleteComment']>('/:commentId', async () => {})
+
+  fastify.patch<CommentsRoute['UpdateComment']>('/commentId', async () => {})
+})
